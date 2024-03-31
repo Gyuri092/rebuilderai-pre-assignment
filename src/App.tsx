1f63b2d84eeb3338aa5f655b284f652f98f768e8
Gyuri092 <ggr0927@gmail.com>
2024-03-31 23:47:10 +0900

    style: App에 구현된 컴포넌트 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css';
+import { MainComponents } from '@components/MainComponents/MainComponents';
 
-function App() {
+const App = () => {
   return (
     <div>
-      멕시코에서 미국 하와이에 걸친 CCZ의 이번 조사는 태평양 심해 해양생태계
-      관찰이 주목적인 ‘SMARTEX 프로젝트’의 일환이다. 드론 촬영에서는 신종 민태과
-      물고기와 온몸이 분홍색인 해삼 동료 바다돼지, 기묘한 형태의 말미잘 및
-      갑각류 등 다양한 생물이 관찰됐다.
+      <MainComponents />
     </div>
   );
-}
+};
 
 export default App;
